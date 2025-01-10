5f34ef11eff3937d3539dd9880786f7c702a28fd
Christopher Siang <chris.siang123@gmail.com>
2025-01-10 01:20:20 +0000

    log Hello World

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 let express = require('express');
 let app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// Log "Hello World" to the console
+console.log("Hello World");
+// Define a route for the root path
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage!');
+  });
+module.exports = app;
